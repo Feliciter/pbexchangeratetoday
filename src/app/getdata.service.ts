9befa399cf81c418a8f09fb528c84207e9287fbb
Feliciter <padakluev@gmx.com>
2020-07-02 23:11:12 +0300

    correct service

--- src/app/getdata.service.ts
+++ src/app/getdata.service.ts
@@ -4,16 +4,6 @@ import { Observable } from 'rxjs';
 import { Pbmodel } from './pbmodel';
 import { DatePipe } from '@angular/common';
 
-// const httpOptions = {
-//   headers: new HttpHeaders({
-//     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Headers': 'Content-Type',
-//     'Access-Control-Allow-Methods': 'GET',
-//     'Access-Control-Allow-Origin': '*'
-//   })
-// };
-// httpOptions.headers =
-//   httpOptions.headers.set( 'Access-Control-Allow-Origin', '*');
 
 @Injectable()
 export class PrivatCurrService {
@@ -33,7 +23,5 @@ export class PrivatCurrService {
     return this.datePipeString;
 
   }
-
-
 
 }
